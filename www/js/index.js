d85addd4da8692e3925d65335bc1c9fa8cd59044
DevWeb13 <giuliano.loic@sfr.fr>
2021-11-21 19:14:57 +0100

    map

---
+++ www/js/index.js
@@ -0,0 +1,2 @@
+import Logo from"../../components/logo/logo.js";class Index{DOM;constructor(o,s){this.DOM=o,this.render()}render(){this.DOM="<h1>test12</h1>",new Logo(this.DOM)}}
+//# sourceMappingURL=index.js.map
